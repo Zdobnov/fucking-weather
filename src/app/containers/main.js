ac9e2ebdb8c6be962421bae5246a9c9bab41decd
Zdobnoff <dr.ankh@gmail.com>
2017-08-30 23:03:49 +0200

    redux added

--- src/app/containers/main.js
+++ src/app/containers/main.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Header from '../components/header';
 
@@ -8,6 +10,10 @@ import Details from './main/components/details';
 import Description from './main/components/description';
 
 export class Main extends Component {
+  static propTypes = {
+    value: PropTypes.number.isRequired
+  }
+
   render() {
     return (
       <div>
@@ -15,6 +21,7 @@ export class Main extends Component {
         <main className="app-main today">
           <section className="weather-today">
             <div className="weather-today__flex--left">
+              {this.props.value}
               <Temperature/>
               <Location
                 location="Minsk, BY"
@@ -33,3 +40,13 @@ export class Main extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    value: state.value
+  };
+};
+
+export default connect(
+  mapStateToProps
+)(Main);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-import {Main} from './app/containers/main';
+import Main from './app/containers/main';
 
 import './index.scss';
 
+// initial state
+const initialState = {
+  value: 100
+};
+
+// reducer
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
+
+// store
+const store = createStore(todoApp);
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}/>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
