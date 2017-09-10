9e36ff789b51973c883b8e28bcb4cd1d64f919fd
Zdobnoff <dr.ankh@gmail.com>
2017-09-10 19:14:14 +0200

    header state added

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
+import classNames from 'classnames';
 
 export default class Header extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      opened: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      opened: !this.state.opened
+    });
+  }
+
   render() {
     return (
-      <header className="app-header">
+      <header
+        className={classNames('app-header', {
+          'app-header--opened': this.state.opened
+        })}
+        >
         <h1 className="app-header__title">FuckingWeather</h1>
-        <div className="app-header__button">
+        <div
+          className="app-header__button"
+          onClick={this.handleClick}
+          >
           <span/>
         </div>
       </header>
