d403d22d6683dc63c4e279e9ea4cd702d6478761
Zdobnov <dr.ankh@gmail.com>
2017-12-24 19:57:04 +0100

    location updated

--- src/app/reducers/currentWeather.js
+++ src/app/reducers/currentWeather.js
@@ -10,7 +10,7 @@ export default function currentWeather(state = initialState, action = {}) {
     case actionTypes.UPDATE_CURRENT_WEATHER: {
       return {
         ...state,
-        current: action.data.current
+        ...action.data
       };
     }
 
